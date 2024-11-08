962f65af7ba65173236b9e292661ac46e60060b7
Pankaj Singh <pnkjsng@amazon.com>
2024-11-08 13:55:26 +0530

    Initial Commit

---
+++ EventCountdown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EventCountdown
+//
+//  Created by Singh, Pankaj on 08/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EventCountdown/EventCountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventCountdownApp.swift
+//  EventCountdown
+//
+//  Created by Singh, Pankaj on 08/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EventCountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
